0a7d0f34506c55219370a2c324b9c2884d80f0c4
Scott <hudsonsc@oregonstate.edu>
2020-07-24 15:29:40 -0600

    Added a timer, prototyped exercise display, added css to do workout.

--- public/scripts/do_workout.js
+++ public/scripts/do_workout.js
@@ -1,5 +1,11 @@
 // Submit button for building a workout
 document.getElementById("doWorkout").addEventListener("click", doWorkout);
+document.getElementById("start").addEventListener("click", startTimer);
+document.getElementById("pause").addEventListener("click", startTimer);
+document.getElementById("end").addEventListener("click", endTimer);
+
+// We'll eventually set this to use the total time of a workout. 
+var timeInterval = 1000;
 
 function doWorkout(){
     var req = new XMLHttpRequest();
@@ -27,4 +33,57 @@ function doWorkout(){
 	  console.error(req.statusText);
 	};
 	req.send(null);
+};
+
+function startTimer(){
+	// Moving forward I'll need the aggregate time of a workout which will be set to start time
+	// I'll need to set the repeater off of start time.
+	// Also seems to be attached to sessions? It certainly persists. Is this bad? 
+
+	// Get the start time and display element
+	var startTime = new Date("Jul 25, 2021 16:37:52").getTime();
+	var display = document.getElementById("time");
+
+	// Repeater function which calculates the remaining time
+	const countDown = setInterval(function() {
+		var rightNow = new Date().getTime();
+		var timeLeft = startTime - rightNow;
+
+		//Calculate minutes and seconds
+		var min = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+		var sec = Math.floor((timeLeft % (1000 * 60)) / (1000));
+
+		// If sec is less than 10 we add a zero to the display otherwise we get 7:9, 7:8
+		if (sec < 10) {
+			sec = "0" + sec
+		}
+		// Sets the text content to the display element
+		display.textContent = min + ":" + sec;
+		
+		// If we're out of time the timer displays complete.
+		// not sure if this will actually stop, it's a new iteration that I haven't tested.
+		if (min <= 0 && sec <= 0) {
+			clearInterval(countDown);
+			display.textContent = "Workout Complete!";
+		}
+	}, timeInterval);
+};
+
+function endTimer(){
+	// can't access due to scope... yay... 
+	timeInterval = null;
+	var display = document.getElementById("time");
+	display.text = "Workout ended";
+};
+
+function displayExercise(rows) {
+	// Pass this the info from the DB, it'll create divs with the appropriate styling
+	// and add them to the DOM.
+	let display = 	document.getElementById("workout-display");
+	let newDiv = document.createElement("div");
+	newDiv.className = "exercises";
+
+	// insert information here at a later time... not feeling it rn.
+
+	display.appendChild(newDiv);
 };
