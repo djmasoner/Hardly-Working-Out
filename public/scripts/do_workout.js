efd2277d7dc994fa275260aa622efd07c8a40493
Ryan Davis <ryancraigdavis@gmail.com>
2020-07-27 19:07:28 -0700

    do workout page now gets the full object of exercises selected in the previous page

--- public/scripts/do_workout.js
+++ public/scripts/do_workout.js
@@ -1,5 +1,5 @@
 // Submit button for building a workout
-document.getElementById("doWorkout").addEventListener("click", displayExercise);
+document.getElementById("doWorkout").addEventListener("click", doWorkout);
 document.getElementById("start").addEventListener("click", startTimer);
 document.getElementById("pause").addEventListener("click", startTimer);
 document.getElementById("end").addEventListener("click", endTimer);
@@ -8,7 +8,31 @@ document.getElementById("end").addEventListener("click", endTimer);
 var timeInterval = 1000;
 
 function doWorkout(){
+    var req = new XMLHttpRequest();
+    var localUrl = 'http://localhost:3000/do_workout';
+    var flipUrl = 'http://flip2.engr.oregonstate.edu:1344/do_workout';
+
+    //req.open('GET', flipUrl, true);
+    req.open('GET', localUrl, true);
     
+    req.withCredentials = false;
+	req.onload = function (e) {
+	  	if (req.readyState === 4) {
+	    	if (req.status === 200) {
+
+	    	// SQL Data returned from server
+            var data = JSON.parse(req.responseText);
+            console.log(data);
+		
+	    	} else {
+	      		console.error(req.statusText);
+	    	}
+	  	}
+	};
+	req.onerror = function (e) {
+	  console.error(req.statusText);
+	};
+	req.send(null);
 };
 
 function startTimer(){
@@ -52,44 +76,14 @@ function endTimer(){
 	display.text = "Workout ended";
 };
 
-function displayExercise() {
+function displayExercise(rows) {
+	// Pass this the info from the DB, it'll create divs with the appropriate styling
+	// and add them to the DOM.
+	let display = 	document.getElementById("workout-display");
+	let newDiv = document.createElement("div");
+	newDiv.className = "exercises";
 
-	var req = new XMLHttpRequest();
-    var localUrl = 'http://localhost:3000/do_workout';
-    var flipUrl = 'http://flip2.engr.oregonstate.edu:1344/do_workout';
+	// insert information here at a later time... not feeling it rn.
 
-    //req.open('GET', flipUrl, true);
-    req.open('GET', localUrl, true);
-    
-    req.withCredentials = false;
-	req.onload = function (e) {
-	  	if (req.readyState === 4) {
-	    	if (req.status === 200) {
-
-	    	// SQL Data returned from server
-            var data = JSON.parse(req.responseText);
-            console.log(data);
-
-            // Pass this the info from the DB, it'll create divs with the appropriate styling
-			// and add them to the DOM.
-			let display = 	document.getElementById("workout-display");
-			let newDiv = document.createElement("div");
-			newDiv.className = "exercises";
-
-			// insert information here at a later time... not feeling it rn.
-
-			display.appendChild(newDiv);
-		
-	    	} else {
-	      		console.error(req.statusText);
-	    	}
-	  	}
-	};
-	req.onerror = function (e) {
-	  console.error(req.statusText);
-	};
-	req.send(null);
-
-
-	
+	display.appendChild(newDiv);
 };
