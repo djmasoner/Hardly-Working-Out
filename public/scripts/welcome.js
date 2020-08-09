6e2e7156b4fd51ff5ec057618c4b06c390f90628
Ryan Davis <ryancraigdavis@gmail.com>
2020-08-08 19:26:30 -0700

    There were two functions in welcome.js that handled accepts and dismisses of challenges. However, I realized I had just copied over the code because i was lazy and didn't make them the same function. I changed the name and combined them, thus fixing a problem of Alternative Classes with Different Interfaces

--- public/scripts/welcome.js
+++ public/scripts/welcome.js
@@ -14,33 +14,7 @@ function hideList() {
     list.style.pointerEvents = "none";
 }
 
-function acceptChallenge(value, id){
-    event.preventDefault();
-
-    var updateObject = new Object();
-    updateObject.id = id;
-    updateObject.accept = value;
-
-    var req = new XMLHttpRequest();
-
-    req.open('POST', serverUrl+'/update_challenge', true);
-    req.setRequestHeader('Content-Type', 'application/json');
-    req.addEventListener('load',function(){
-      if(req.status >= 200 && req.status < 400){
-      	if (value == 1) {
-      		console.log('Challenge Accepted!')
-      	} else {
-      		console.log('Challenge Declined')
-      	};
-      } else {
-        console.log("Error in network request: " + req.statusText);
-      }});
-
-    req.send(JSON.stringify(updateObject));
-
-};
-
-function dismissChallenge(value, id){
+function chooseChallenge(value, id){
     event.preventDefault();
 
     var updateObject = new Object();
@@ -80,7 +54,7 @@ function getActiveChallenges(){
             var data = JSON.parse(req.responseText);
             for (var i = 0; i < data.length; i++) {
             	var date = (data[i].end_date).substring(0,10);
-            	document.getElementById('createChallengeModal').innerHTML += '<div class="modal fade" id="newChallenge'+data[i].challenge_id+'" tabindex="-1" role="dialog" aria-labelledby="newChallengeLabel" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="newChallengeLabel">New Challenge!</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body" id="challengeModalBody">'+data[i].username+' is challenging you! You will have until '+date+' to complete the challenge. Do you accept?</div><div class="modal-footer"><button type="button" class="btn btn-secondary" value="2" name="'+data[i].challenge_id+'" onclick="dismissChallenge(this.value,this.name)"  data-dismiss="modal">Dismiss Challenge</button><button type="button" class="btn btn-primary" value="1" name="'+data[i].challenge_id+'" onclick="acceptChallenge(this.value,this.name)" data-dismiss="modal">Accept Challenge</button></div></div></div></div>'
+            	document.getElementById('createChallengeModal').innerHTML += '<div class="modal fade" id="newChallenge'+data[i].challenge_id+'" tabindex="-1" role="dialog" aria-labelledby="newChallengeLabel" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="newChallengeLabel">New Challenge!</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body" id="challengeModalBody">'+data[i].username+' is challenging you! You will have until '+date+' to complete the challenge. Do you accept?</div><div class="modal-footer"><button type="button" class="btn btn-secondary" value="2" name="'+data[i].challenge_id+'" onclick="chooseChallenge(this.value,this.name)"  data-dismiss="modal">Dismiss Challenge</button><button type="button" class="btn btn-primary" value="1" name="'+data[i].challenge_id+'" onclick="chooseChallenge(this.value,this.name)" data-dismiss="modal">Accept Challenge</button></div></div></div></div>'
             	$('#newChallenge'+data[i].challenge_id).modal('show')
 
             };
